5b8e3c3e50c102052c79334c9118509cff46ce4a
Arashdeep <arashdeep@d142-058-222-245.wireless.sfu.ca>
2025-03-06 15:55:54 -0800

    newer changes

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from .forms import SignupForm, LoginForm
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
+    path('contact/', views.contacts, name='contact'),
     path('shop/', views.product_list, name='product_list'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_detail, name='cart_detail'),
