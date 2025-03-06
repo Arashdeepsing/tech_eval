f87b0a3d07dcfa41e5a8b7ba7a1dd0ad5dbd94b0
Arashdeep <arashdeep@d142-058-222-245.wireless.sfu.ca>
2025-03-06 15:15:51 -0800

    New Changes

--- ecommercePrj/settings.py
+++ ecommercePrj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x&hf=1evoa#ay54w+r(%!t)^b6x^%-r@t=q11&(o05o(xsrets'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://tech-eval-three.vercel.app']
+ALLOWED_HOSTS = ['https://tech-eval-three.vercel.app','localhost','127.0.0.1']
 
 
 # Application definition
