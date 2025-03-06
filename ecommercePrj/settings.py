ebf2f24ed041f529f01506d3319ca5b9d5a02a48
Arashdeep <arashdeep@d142-058-222-245.wireless.sfu.ca>
2025-03-06 14:08:57 -0800

    New changes for production

--- ecommercePrj/settings.py
+++ ecommercePrj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x&hf=1evoa#ay54w+r(%!t)^b6x^%-r@t=q11&(o05o(xsrets'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
